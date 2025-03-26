959b585648e653f3928eb3ec88e562566184210f
loneprog <wolfdon73@gmail.com>
2025-03-26 16:41:21 +0000

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p className="text-3xl font-bold underline bg-blue-200">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
